4b28f1f288efb84ece0128ab69bfb4d8a02123fd
Vladimir RM <shvdrummer@gmail.com>
2022-10-13 13:48:08 +0300

    add classes for component button

---
+++ src/Components/Button/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const borderStyles = 'border-2 border-almost-black rounded-lg '
+const filledStyles = 'text-almost-white bg-black rounded-lg font-bold px-6 py-4 mx-0 hover:bg-transparent hover:text-almost-black border-2 border-almost-black'
+const Button = ({
+
+    children = "",
+    hasBorder = false,
+    isFilled = false,
+}) => {
+  return (
+    <button className={`text-medium-grey px-5 py-2 ${hasBorder && borderStyles} ${isFilled}`}>{children}</button>
+  )
+}
+
+export default Button
