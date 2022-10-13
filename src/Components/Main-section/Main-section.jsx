0baf28ebd2662b2a7b9e482fc1af89c66e11a1f7
Vladimir RM <shvdrummer@gmail.com>
2022-10-13 19:53:27 +0300

    adding rirht section img

--- src/Components/Main-section/Main-section.jsx
+++ src/Components/Main-section/Main-section.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import MenImageMobile from '../../images/image-hero-mobile.png'
 import Button from '../Button'
+import MenImage from '../../images/image-hero-desktop.png';
+
 
 import { ReactComponent as Databiz } from '../../images/client-databiz.svg'
 import { ReactComponent as Audiophile } from '../../images/client-audiophile.svg'
@@ -9,19 +11,23 @@ import { ReactComponent as Maker } from '../../images/client-maker.svg'
 
 const MainSection = () => {
     return (
-        <section className='w-full flex mt-6  h-screen'>
+        <section className='w-full flex mt-6  h-screen justify-between'>
             <div className='relative text-left mt-12'>
                 <h1 className="text-8xl font-black mt-60 whitespace-pre-line">{`Make\n remote work`}</h1>
                 <p className='text-medium-gray text-lg my-12 whitespace-pre-line'>
                     {`Lorem ipsum dolor, sit amet consectetur adipisicing elit.\n Amet ullam dolorum animi atque vitae exercitationem\n quibusdam maiores adipisci qui harum.`}
                 </p>
                 <Button isFilled={true}>Learn more</Button>
-                <div className="flex justify-around absolute bottom-2">
+                <div className=" flex justify-around  bottom-2 w-full">
+
                     <Databiz />
                     <Audiophile />
                     <Meet />
                     <Maker />
                 </div>
+            </div>
+            <div className="flex w-2/4 mt-20">
+                <img src={MenImage} alt='Men'/>
             </div>
         </section>
     )
