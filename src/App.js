a326d99dd2c37a607a69fc6f6de71b227f4bfc80
Vladimir RM <shvdrummer@gmail.com>
2022-10-13 17:05:18 +0300

    ad Button in Main-section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Wrapper from "./Components/wrapper/index";
 import Header from "./Components/Header";
+import MainSection from "./Components/Main-section/Main-section";
 
 function App() {
   return (
     <Wrapper>
       <Header />
+      <MainSection />
     </Wrapper>
   );
 }
