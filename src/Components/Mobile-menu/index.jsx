9cb7b1ac8b4925b19270c81ea3098cf93e718134
Vladimir RM <shvdrummer@gmail.com>
2022-10-14 14:21:55 +0300

    create second part mob-menu

--- src/Components/Mobile-menu/index.jsx
+++ src/Components/Mobile-menu/index.jsx
@@ -3,10 +3,13 @@ import React from 'react'
 export const MobilMenu = ({ isOpen = false
 }) => {
     return (
-        <React.Fragment>
+        <>
       <div className={`absolute top-0 left-0 right-0 bg-almost-black opacity-50 z-10 min-h-[150vh] ${isOpen ? 'flex' : 'hidden'}`} />
+      <div className={`absolute right-0 top-0 w-1/2 bg-white z-20 justify-center min-h-[150vh] ${isOpen ? 'flex' : 'hidden'}`}>
 
-        </React.Fragment>
+      </div>
+
+        </>
     )
 }
 
