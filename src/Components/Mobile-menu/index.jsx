82551594668936a75069f91ac36013c571c24d62
Vladimir RM <shvdrummer@gmail.com>
2022-10-14 14:08:27 +0300

    create opacity gor mobile menu

--- src/Components/Mobile-menu/index.jsx
+++ src/Components/Mobile-menu/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
- export const MobilMenu = ({isOpen = false
+export const MobilMenu = ({ isOpen = false
 }) => {
-  return (
-    <React.Fragment>MobilMenu</React.Fragment>
-  )
+    return (
+        <React.Fragment>
+      <div className={`absolute top-0 left-0 right-0 bg-almost-black opacity-50 z-10 min-h-[150vh] ${isOpen ? 'flex' : 'hidden'}`} />
+
+        </React.Fragment>
+    )
 }
 
 export default MobilMenu
