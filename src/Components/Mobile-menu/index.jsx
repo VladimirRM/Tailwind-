650d685713aa97d3a924176cc5a53573a03a3db0
Vladimir RM <shvdrummer@gmail.com>
2022-10-15 09:14:19 +0300

    adding new classes for  mob-menu

--- src/Components/Mobile-menu/index.jsx
+++ src/Components/Mobile-menu/index.jsx
@@ -6,7 +6,7 @@ export const MobilMenu = ({ isOpen = false
         <>
       <div className={`absolute top-0 left-0 right-0 bg-almost-black opacity-50 z-10 min-h-[150vh] ${isOpen ? 'flex' : 'hidden'}`} />
       <div className={`absolute right-0 top-0 w-1/2 bg-white z-20 justify-center min-h-[150vh] ${isOpen ? 'flex' : 'hidden'}`}>
-
+        <nav className="my-20 mx-10"></nav>
       </div>
 
         </>
