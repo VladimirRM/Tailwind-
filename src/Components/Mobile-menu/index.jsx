b9c4a3f3a79e61034d6ee97416c76491ede56e39
Vladimir RM <shvdrummer@gmail.com>
2022-10-15 09:19:27 +0300

    adding text for MenuItem

--- src/Components/Mobile-menu/index.jsx
+++ src/Components/Mobile-menu/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import NavItem from '../Nav-item'
 
 export const MobilMenu = ({ isOpen = false
 }) => {
@@ -6,7 +7,9 @@ export const MobilMenu = ({ isOpen = false
         <>
       <div className={`absolute top-0 left-0 right-0 bg-almost-black opacity-50 z-10 min-h-[150vh] ${isOpen ? 'flex' : 'hidden'}`} />
       <div className={`absolute right-0 top-0 w-1/2 bg-white z-20 justify-center min-h-[150vh] ${isOpen ? 'flex' : 'hidden'}`}>
-        <nav className="my-20 mx-10"></nav>
+        <nav className="my-20 mx-5 space-x-5 text-lg w-full">
+          <NavItem text='Features'/>
+        </nav>
       </div>
 
         </>
